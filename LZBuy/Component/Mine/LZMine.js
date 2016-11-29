4ba78868118617a857fbb699bc41fe59db364a09
zhen.lei <zhen.lei@kindroid.com>
2016-11-29 23:40:57 +0800

    init

---
+++ LZBuy/Component/Mine/LZMine.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+var Main = React.createClass({
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    我的!
+                </Text>
+            </View>
+        );
+    }
+});
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+});
+
+// 输出组件类
+modulo.exports = Main;
